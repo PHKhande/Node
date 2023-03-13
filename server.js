52a0986372d6028f26ff28323575784f939ae0b0
PHKhande <phk151raj@gmail.com>
2023-03-13 21:40:17 +0530

    Response for the incoming requests

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const http = require('http');
 
 const server = http.createServer( (req, res) => {
-    console.log(req);
-    process.exit();
+    res.setHeader('Content-Type', 'text/html'); 
+    res.write('<html>');
+    res.write('<h1> Server </h1>');
+    res.write('<p> Server Paragraph</p>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
