2243cf93a36bb3f6cda7f383800a5241f4090cde
PHKhande <phk151raj@gmail.com>
2023-03-14 16:00:51 +0530

    Parsing requests and writing into a file

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer( (req, res) => {
-    res.setHeader('Content-Type', 'text/html'); 
-    res.write('<html>');
-    res.write('<h1> Server </h1>');
-    res.write('<p> Server Paragraph</p>');
-    res.write('</html>');
-    res.end();
+    const url = req.url;
+    const method = req.method;
+    const body = [];
+    if(url === '/'){
+        res.write('<body><form action = "/message" method = "POST"> <input type="text" name="message"> <button type="submit"> SEND </button> </form></body>');
+        res.end();
+    }
+    else if(url === '/message' && method === 'POST'){
+        req.on('data', chunk => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            fs.writeFile('message.txt', parsedBody, ()=> {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                res.end();
+            })
+        })
+        
+    }
 });
 
 server.listen(3000);
