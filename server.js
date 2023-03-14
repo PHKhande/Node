089aa1acc27db75574bd55ee1444240981872e44
PHKhande <phk151raj@gmail.com>
2023-03-14 16:19:46 +0530

    Using node module systems and using routes

--- server.js
+++ server.js
@@ -1,30 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer( (req, res) => {
-    const url = req.url;
-    const method = req.method;
-    const body = [];
-    if(url === '/' && method === 'GET'){
-        const show = fs.readFileSync('message.txt');
-        res.write(`<body> <h4>${show} </h4>`)
-        res.write('<form action = "/" method = "POST"> <input type="text" name="message"> <button type="submit"> SEND </button> </form></body>');
-        res.end();
-    }
-    else if(url === '/' && method === 'POST'){
-        req.on('data', chunk => {
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            fs.writeFile('message.txt', parsedBody.split('=')[1], () => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                res.end();
-            })
-        })
-        
-    }
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
