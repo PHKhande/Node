5b44c4f69b60d9a25e834332a3e0db2751802a7e
PHKhande <phk151raj@gmail.com>
2023-03-14 19:43:16 +0530

    Handling routes, parsing requests&limiting to post

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use( (req, res, next) => {
-    console.log("In middleware");
-    next();
+app.use(bodyParser.urlencoded( {extended:false} ));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">SEND</button></form>')
 });
 
-app.use( (req, res, next) => {
-    console.log("In second middleware");
-    res.send('<h1> Hello </h1>')
+app.post( '/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+});
+
+app.use( '/', (req, res, next) => {
+    res.send('<h1> Home </h1>')
 });
 
 app.listen(3000);
