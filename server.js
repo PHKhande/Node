043ae665bd5e988428fb4c7f8c69a61025fea5af
PHKhande <phk151raj@gmail.com>
2023-03-14 21:20:39 +0530

    Raw chat app

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const chatRoutes = require('./routes/chatApp');
+
 
 app.use(bodyParser.urlencoded( {extended:false} ));
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
+app.use(chatRoutes);
+
 
 app.use((req, res, next) => {
     res.status(404).send('<h1>Page not found</h1>');
