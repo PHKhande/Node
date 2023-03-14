0312811fec8244737b09ae13816b24942a413a9f
PHKhande <phk151raj@gmail.com>
2023-03-14 16:11:48 +0530

    Retrieving last sent message and showing in page

--- server.js
+++ server.js
@@ -5,17 +5,19 @@ const server = http.createServer( (req, res) => {
     const url = req.url;
     const method = req.method;
     const body = [];
-    if(url === '/'){
-        res.write('<body><form action = "/message" method = "POST"> <input type="text" name="message"> <button type="submit"> SEND </button> </form></body>');
+    if(url === '/' && method === 'GET'){
+        const show = fs.readFileSync('message.txt');
+        res.write(`<body> <h4>${show} </h4>`)
+        res.write('<form action = "/" method = "POST"> <input type="text" name="message"> <button type="submit"> SEND </button> </form></body>');
         res.end();
     }
-    else if(url === '/message' && method === 'POST'){
+    else if(url === '/' && method === 'POST'){
         req.on('data', chunk => {
             body.push(chunk);
         });
         req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
-            fs.writeFile('message.txt', parsedBody, ()=> {
+            fs.writeFile('message.txt', parsedBody.split('=')[1], () => {
                 res.statusCode = 302;
                 res.setHeader('Location', '/');
                 res.end();
